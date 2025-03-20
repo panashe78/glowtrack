76522004c0f540d7e2649abbac141063b9ebc64e
panashe78 <161289848+panashe78@users.noreply.github.com>
2025-03-20 09:10:04 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -174,12 +174,12 @@
                   @foreach ($auctions as $auction)
                   <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow  ">
                     <a href="#">
-                        <div class="rounded-t-lg bg-cover bg-center h-72"  style="background-image: url(images/boat.jpg)"></div>
+                        <div class="rounded-t-lg bg-cover bg-center h-72"  style="background-image: url('/storage/{{$auction->coverphoto }}')"></div>
 
                     </a>
                     <div class="px-5 pb-5">
                         <a href="#">
-                            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Boat</h5>
+                            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $auction->product }}</h5>
                         </a>
                         <div class="flex items-center mt-2.5 mb-5">
                             <div class="flex items-center space-x-1 rtl:space-x-reverse">
@@ -202,7 +202,7 @@
                             <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
                         </div>
                         <div class="flex items-center justify-between">
-                            <span class="text-3xl font-bold text-gray-900 dark:text-white">$20.000</span>
+                            <span class="text-3xl font-bold text-gray-900 dark:text-white">${{ $auction->starting_bid }}</span>
                             <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
                         </div>
                     </div>
